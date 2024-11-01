c24fc82be3b2a4e6f91d1dbcbcd1ad5b15afd4bf
SURU SRI MUKHESH <108520098+SriMukheshSuru@users.noreply.github.com>
2024-11-01 16:19:18 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import {Route} from 'react-router-dom';
-import homepage from "./pages/Homepage";
-import chatpage from "./pages/chatpage";
+import { BrowserRouter, Route } from 'react-router-dom';
+import homepage from './pages/Homepage';
+import chatpage from './pages/chatpage';
 
 function App() {
   return (
-    <div className="App">
-      <Route path="/" component={homepage} exact/>
-      <Route path="/chats" component ={chatpage} />
-    </div>
+    <BrowserRouter basename="/olalaa">
+      <div className="App">
+        <Route path="/" component={homepage} exact />
+        <Route path="/chats" component={chatpage} />
+      </div>
+    </BrowserRouter>
   );
 }
 
